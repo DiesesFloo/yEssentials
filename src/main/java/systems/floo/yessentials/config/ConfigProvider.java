9873531dea78e382cf7a8dd2f90a033875da8327
Floo <fmanice07@gmail.com>
2022-11-29 23:09:09 +0100

    feat(plugin): Added ConfigProvider

---
+++ src/main/java/systems/floo/yessentials/config/ConfigProvider.java
@@ -0,0 +1,33 @@
+package systems.floo.yessentials.config;
+
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+import systems.floo.yessentials.EssentialsPlugin;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ConfigProvider {
+
+    private static final JavaPlugin PLUGIN = EssentialsPlugin.getPlugin();
+
+    public static YamlConfiguration getCustomConfig(String path) {
+        File configFile = new File(PLUGIN.getDataFolder(), path);
+
+        if (!configFile.exists()){
+            PLUGIN.saveResource(path, false);
+        }
+
+        YamlConfiguration config = new YamlConfiguration();
+
+        try {
+            config.load(configFile);
+        } catch (IOException | InvalidConfigurationException e){
+            e.printStackTrace();
+        }
+
+        return config;
+    }
+
+}
---
+++ src/main/java/systems/floo/yessentials/config/ConfigStartupListener.java
@@ -0,0 +1,16 @@
+package systems.floo.yessentials.config;
+
+import org.bukkit.plugin.java.JavaPlugin;
+import systems.floo.yessentials.EssentialsPlugin;
+import systems.floo.yessentials.instances.interfaces.IStartupHandler;
+
+public class ConfigStartupListener implements IStartupHandler {
+
+    private static final JavaPlugin PLUGIN = EssentialsPlugin.getPlugin();
+
+    @Override
+    public void handleStart() {
+
+    }
+
+}
