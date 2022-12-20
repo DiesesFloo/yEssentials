0f1e545a2a34d3bd827f8454079edec4ac825168
Floo <fmanice07@gmail.com>
2022-12-20 16:51:40 +0100

    feat(plugin): Added vanish command

---
+++ src/main/java/systems/floo/yessentials/commands/vanish/VanishCommand.java
@@ -0,0 +1,75 @@
+package systems.floo.yessentials.commands.vanish;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import systems.floo.yessentials.messages.MessageProvider;
+
+import java.util.Arrays;
+
+public class VanishCommand extends Command {
+    public VanishCommand() {
+        super("vanish",
+                "Sets a player in vanish mode",
+                "",
+                Arrays.asList(new String[]{"v", "hideplayer", "vanishplayer"}));
+    }
+
+    @Override
+    public boolean execute(CommandSender sender, String commandLabel, String[] args) {
+        if (args.length >= 1){
+            if (!sender.hasPermission("essentials.vanish.others")) {
+                sender.sendMessage(MessageProvider.getMessage("noperm"));
+                return false;
+            }
+
+            Player target = Bukkit.getPlayer(args[0]);
+
+            if (target == null) {
+                sender.sendMessage(MessageProvider.getMessage("playernotfound"));
+                return false;
+            }
+
+            if (VanishCommandProvider.isVanished(target)){
+                VanishCommandProvider.unvanishPlayer(target);
+
+                sender.sendMessage(MessageProvider.getMessage("unvanishothers")
+                        .replaceAll("%player%", target.getDisplayName()));
+                target.sendMessage(MessageProvider.getMessage("unvanishself")
+                        .replaceAll("%player%", target.getDisplayName()));
+
+                return true;
+            }
+
+            VanishCommandProvider.vanishPlayer(target);
+
+            sender.sendMessage(MessageProvider.getMessage("vanishothers")
+                    .replaceAll("%player%", target.getDisplayName()));
+            target.sendMessage(MessageProvider.getMessage("vanishself")
+                    .replaceAll("%player%", target.getDisplayName()));
+
+            return true;
+        }
+
+        if (!(sender instanceof Player)){
+            return false;
+        }
+
+        Player player = (Player) sender;
+
+        if (VanishCommandProvider.isVanished(player)){
+            VanishCommandProvider.unvanishPlayer(player);
+            player.sendMessage(MessageProvider.getMessage("unvanishself")
+                    .replaceAll("%player%", player.getDisplayName()));
+
+            return true;
+        }
+
+        VanishCommandProvider.vanishPlayer(player);
+        player.sendMessage(MessageProvider.getMessage("vanishself")
+                .replaceAll("%player%", player.getDisplayName()));
+
+        return true;
+    }
+}
---
+++ src/main/java/systems/floo/yessentials/commands/vanish/VanishCommandProvider.java
@@ -0,0 +1,63 @@
+package systems.floo.yessentials.commands.vanish;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+import systems.floo.yessentials.EssentialsPlugin;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class VanishCommandProvider {
+
+    private static final JavaPlugin PLUGIN = EssentialsPlugin.getPlugin();
+    private static ArrayList<UUID> vanishedPlayers = new ArrayList<>();
+
+    /**
+     * Returns the list of the players in fly mode
+     *
+     * @return List of players in fly mode
+     */
+    public static ArrayList<UUID> getVanishedPlayers() {
+        return vanishedPlayers;
+    }
+
+    /**
+     * Checks if a player is in fly mode
+     *
+     * @param p The player to check if he's in fly mode
+     * @return Returns if the player is in fly mode
+     */
+    public static boolean isVanished(Player p) {
+        return vanishedPlayers.contains(p.getUniqueId());
+    }
+
+    /**
+     * Sets a player into vanish mode
+     *
+     * @param p The player to vanish
+     */
+    public static void vanishPlayer(Player p) {
+        vanishedPlayers.add(p.getUniqueId());
+        for (Player all : Bukkit.getOnlinePlayers()){
+            if (all.hasPermission("essentials.vanish.bypass")){
+                continue;
+            }
+
+            all.hidePlayer(PLUGIN, p);
+        }
+    }
+
+    /**
+     * Unvanishs a player who is in vanish mode
+     *
+     * @param p The player to unvanish
+     */
+    public static void unvanishPlayer(Player p) {
+        vanishedPlayers.remove(p.getUniqueId());
+        for (Player all: Bukkit.getOnlinePlayers()){
+            all.showPlayer(PLUGIN, p);
+        }
+    }
+
+}
