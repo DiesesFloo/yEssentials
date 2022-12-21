cbabb22ceda1b67f52bf47de90744f54d83cef48
Floo <fmanice07@gmail.com>
2022-12-21 09:58:08 +0100

    feat(plugin): Added methods for placeholder usage in message provider

--- src/main/java/systems/floo/yessentials/messages/MessageProvider.java
+++ src/main/java/systems/floo/yessentials/messages/MessageProvider.java
@@ -2,7 +2,9 @@ package systems.floo.yessentials.messages;
 
 import org.bukkit.ChatColor;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
 import systems.floo.yessentials.config.ConfigProvider;
+import systems.floo.yessentials.economy.EconomyProvider;
 
 public class MessageProvider {
 
@@ -55,6 +57,122 @@ public class MessageProvider {
 
     /**
      * Returns the message defined with the key
+     * in params from the config and replaces given
+     * placeholders
+     *
+     * @param key The key of the message
+     * @param name1 The name of the command executor
+     * @param name2 The name of the command target
+     * @return The message with the given key and main prefix
+     */
+    public static String getMessage(String key, String name1, String name2) {
+        return getMessage(key)
+                .replaceAll("%player1%", name1)
+                .replaceAll("%player2%", name2);
+    }
+
+    /**
+     * Returns the message defined with the key
+     * in params from the config and replaces given
+     * placeholders
+     *
+     * @param key The key of the message
+     * @param player1 The command executor
+     * @param player2 The command target
+     * @return The message with the given key and main prefix
+     */
+    public static String getMessage(String key, Player player1, Player player2) {
+        return getMessage(key, player1.getName(), player2.getName());
+    }
+
+    /**
+     * Returns the message defined with the key
+     * in params from the config and replaces given
+     * placeholders
+     *
+     * @param key The key of the message
+     * @param name The name of the command executor
+     * @return The message with the given key and main prefix
+     */
+    public static String getMessage(String key, String name) {
+        return getMessage(key).replaceAll("%player%", name);
+    }
+
+    /**
+     * Returns the message defined with the key
+     * in params from the config and replaces given
+     * placeholders
+     *
+     * @param key The key of the message
+     * @param player The command executor
+     * @return The message with the given key and main prefix
+     */
+    public static String getMessage(String key, Player player) {
+        return getMessage(key, player.getName());
+    }
+
+    /**
+     * Returns the message defined with the key
+     * in params from the config and replaces given
+     * placeholders
+     *
+     * @param key The key of the message
+     * @param name The name of the command executor
+     * @param amount The amount of money
+     * @return The message with the given key and main prefix
+     */
+    public static String getMessage(String key, String name, double amount) {
+        return getMessage(key, name)
+                .replaceAll("%amount%", amount + EconomyProvider.getCurrencySign());
+    }
+
+    /**
+     * Returns the message defined with the key
+     * in params from the config and replaces given
+     * placeholders
+     *
+     * @param key The key of the message
+     * @param player The command executor
+     * @param amount The amount of money
+     * @return The message with the given key and main prefix
+     */
+    public static String getMessage(String key, Player player, double amount) {
+        return getMessage(key, player.getName(), amount);
+    }
+
+    /**
+     * Returns the message defined with the key
+     * in params from the config and replaces given
+     * placeholders
+     *
+     * @param key The key of the message
+     * @param name1 The name of the command executor
+     * @param name2 The name of the command target
+     * @param amount The amount of money
+     * @return The message with the given key and main prefix
+     */
+    public static String getMessage(String key, String name1, String name2, double amount) {
+        return getMessage(key, name1, name2)
+                .replaceAll("%amount%", amount + EconomyProvider.getCurrencySign());
+    }
+
+    /**
+     * Returns the message defined with the key
+     * in params from the config and replaces given
+     * placeholders
+     *
+     * @param key The key of the message
+     * @param player1 The command executor
+     * @param player2 The command target
+     * @param amount The amount of money
+     * @return The message with the given key and main prefix
+     */
+    public static String getMessage(String key, Player player1, Player player2, double amount) {
+        return getMessage(key, player1.getName(), player2.getName(), amount);
+    }
+
+    /**
+     * Returns the message defined with the key
      * in params from the config and with the
      * given prefix type
      *
@@ -68,4 +186,136 @@ public class MessageProvider {
                 CONFIG.getString("strings.messages." + key));
     }
 
+    /**
+     * Returns the message defined with the key
+     * in params from the config and with the
+     * given prefix type and replaces given
+     * placeholders
+     *
+     * @param prefixType The type of the prefix
+     * @param key The key of the message
+     * @param name1 The name of the command executor
+     * @param name2 The name of the command target
+     * @return The message with the given key and given prefix
+     */
+    public static String getMessage(PrefixType prefixType, String key, String name1, String name2) {
+        return getMessage(prefixType, key)
+                .replaceAll("%player1%", name1)
+                .replaceAll("%player2%", name2);
+    }
+
+    /**
+     * Returns the message defined with the key
+     * in params from the config and with the
+     * given prefix type and replaces given
+     * placeholders
+     *
+     * @param prefixType The type of the prefix
+     * @param key The key of the message
+     * @param player1 The command executor
+     * @param player2 The command target
+     * @return The message with the given key and given prefix
+     */
+    public static String getMessage(PrefixType prefixType, String key, Player player1, Player player2) {
+        return getMessage(prefixType, key, player1.getName(), player2.getName());
+    }
+
+    /**
+     * Returns the message defined with the key
+     * in params from the config and with the
+     * given prefix type and replaces given
+     * placeholders
+     *
+     * @param prefixType The type of the prefix
+     * @param key The key of the message
+     * @param name The name of the command executor
+     * @return The message with the given key and given prefix
+     */
+    public static String getMessage(PrefixType prefixType, String key, String name) {
+        return getMessage(prefixType, key).replaceAll("%player%", name);
+    }
+
+    /**
+     * Returns the message defined with the key
+     * in params from the config and with the
+     * given prefix type and replaces given
+     * placeholders
+     *
+     * @param prefixType The type of the prefix
+     * @param key The key of the message
+     * @param player The command executor
+     * @return The message with the given key and given prefix
+     */
+    public static String getMessage(PrefixType prefixType, String key, Player player) {
+        return getMessage(prefixType, key, player.getName());
+    }
+
+    /**
+     * Returns the message defined with the key
+     * in params from the config and with the
+     * given prefix type and replaces given
+     * placeholders
+     *
+     * @param prefixType The type of the prefix
+     * @param key The key of the message
+     * @param name The name of the command executor
+     * @param amount The amount of money
+     * @return The message with the given key and given prefix
+     */
+    public static String getMessage(PrefixType prefixType, String key, String name, double amount) {
+        return getMessage(prefixType, key, name)
+                .replaceAll("%amount%", amount + EconomyProvider.getCurrencySign());
+    }
+
+    /**
+     * Returns the message defined with the key
+     * in params from the config and with the
+     * given prefix type and replaces given
+     * placeholders
+     *
+     * @param prefixType The type of the prefix
+     * @param key The key of the message
+     * @param player The command executor
+     * @param amount The amount of money
+     * @return The message with the given key and given prefix
+     */
+    public static String getMessage(PrefixType prefixType, String key, Player player, double amount) {
+        return getMessage(prefixType, key, player.getName(), amount);
+    }
+
+    /**
+     * Returns the message defined with the key
+     * in params from the config and with the
+     * given prefix type and replaces given
+     * placeholders
+     *
+     * @param prefixType The type of the prefix
+     * @param key The key of the message
+     * @param name1 The name of the command executor
+     * @param name2 The name of the command target
+     * @param amount The amount of money
+     * @return The message with the given key and given prefix
+     */
+    public static String getMessage(PrefixType prefixType, String key, String name1, String name2, double amount) {
+        return getMessage(prefixType, key, name1, name2)
+                .replaceAll("%amount%", amount + EconomyProvider.getCurrencySign());
+    }
+
+    /**
+     * Returns the message defined with the key
+     * in params from the config and with the
+     * given prefix type and replaces given
+     * placeholders
+     *
+     * @param prefixType The type of the prefix
+     * @param key The key of the message
+     * @param player1 The command executor
+     * @param player2 The command target
+     * @param amount The amount of money
+     * @return The message with the given key and given prefix
+     */
+    public static String getMessage(PrefixType prefixType, String key, Player player1, Player player2, double amount) {
+        return getMessage(prefixType, key, player1.getName(), player2.getName(), amount);
+
+    }
 }
